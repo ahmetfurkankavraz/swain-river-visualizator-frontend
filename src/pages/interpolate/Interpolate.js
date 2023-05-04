2bb7c92c872c39c0809ba19e60fed4da8aeb72d7
Ahmet Furkan Kavraz <ahmetfurkankavraz@hotmail.com>
2023-05-04 13:05:21 +0300

    change the layout of interpolation and add scale related components

--- src/pages/interpolate/Interpolate.js
+++ src/pages/interpolate/Interpolate.js
@@ -8,9 +8,18 @@ import SelectType from './SelectType';
 import LoadingMessage from '../../components/LoadingMessage';
 import MeasurementMarker from './MeasurementMarker';
 
+function arrayRange(start, end, step = 1) {
+    const arr = [];
+    for (let i = start; i <= end; i += step) {
+      arr.push(i);
+    }
+    return arr;
+}
+  
+
 const mapContainerStyle = {
     height: "500px",
-    width: "800px"
+    width: "600px"
 }
 
 function Interpolate({setLoggedOut, onLogout}) {
@@ -23,6 +32,21 @@ function Interpolate({setLoggedOut, onLogout}) {
     let [isLoading, setIsLoading] = useState(false);
     let [measurements, setMeasurements] = useState(null);
     let [measurementVisibility, setMeasurementVisibility] = useState(false);
+
+    let [scaleList, setScaleList] = useState([0, 0, 0, 0, 0, 0]);
+    let [buttonClicked, setButtonClicked] = useState(false);
+
+    const handleInputChange = (event, index) => {
+        const newList = [...scaleList];
+        if (event.target.value === '') {
+            newList[index] = 0;
+            setScaleList(newList);
+            return;
+        }
+        newList[index] = parseFloat(event.target.value);
+        setScaleList(newList);
+        console.log(newList);
+    };
 
     useEffect(() => {
         const token = localStorage.getItem('token');
@@ -60,6 +84,46 @@ function Interpolate({setLoggedOut, onLogout}) {
     useEffect(() => {
         const token = localStorage.getItem('token');
         const controller = new AbortController()
+
+        if (!selectedDate && !selectedType) {
+            return
+        }
+
+        setScaleList([0, 0, 0, 0, 0, 0]);
+
+        fetch('/measurement/catalog/' + selectedDate + '/' + selectedType, {
+            signal: controller.signal,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }})
+            .then(res => {
+                if (res.status === 401 || res.status === 403) {
+                    setLoggedOut();
+                    localStorage.removeItem('token');
+                }
+                return res.json();
+            })
+            .then(json => {
+                let min = json["min-value"];
+                let max = json["max-value"];
+                let scaleList = arrayRange(min, max, (max-min)/5);
+                setScaleList(scaleList);
+            })
+            .catch(error => {
+                if (error.name === "SyntaxError") {
+                    alert('There was an error fetching data from the server. Please try again later.');
+                    setLoggedOut();
+                    localStorage.removeItem('token');
+                }
+            })
+        
+        return () => controller.abort()
+    }, [selectedType])
+
+    const retrieveMeasurements = async () => {
+        const token = localStorage.getItem('token');
+        const controller = new AbortController()
         setMeasurements(null);
         if (selectedDate && selectedType) {
             fetch('/measurement/' + selectedDate + '/' + selectedType, {
@@ -88,9 +152,9 @@ function Interpolate({setLoggedOut, onLogout}) {
                 })
         }
         return () => controller.abort()
-    }, [selectedType])
+    }
 
-    useEffect(() => {
+    const retrieveInterpolation = async () => {
         const token = localStorage.getItem('token');
         const controller = new AbortController()
         setInterpolatedRiver(null);
@@ -125,7 +189,7 @@ function Interpolate({setLoggedOut, onLogout}) {
         }
 
         return () => controller.abort()
-    }, [selectedType]);
+    }
 
     useEffect(() => {
         const token = localStorage.getItem('token');
@@ -163,45 +227,90 @@ function Interpolate({setLoggedOut, onLogout}) {
         // Do something with the "types" state here, if needed.
     }, [types]);
 
+    const handleInterpolation = async () => {
+        retrieveInterpolation();
+        retrieveMeasurements();
+    }
+
     return (
         <div>
             <h1>Interpolate</h1>
             <Navbar logOut={onLogout}/>
             {dates && (
                 <div className='interpolation-form'>
-                    <div>
-                        <SelectDate dates={dates} 
-                            setSelectedDate={setSelectedDate} />
+                    <div className='selected-date-type'>
+                        <div>
+                            <SelectDate dates={dates} 
+                                setSelectedDate={setSelectedDate} />
+                        </div>
+                        <br />
+                        <div>
+                            {types && <SelectType types={types}
+                                setSelectedType={setSelectedType} />}
+                        </div>
                     </div>
-                    <br />
+                    <div className='select-ranges margin-horizontal-20'>
+                    {selectedDate && selectedType && (
+                        <div className='range-form' style={{flexDirection:'row'}}>
+                            <div>
+                                <label>Ranges</label>
+                            </div>
+                            <div style={{flexDirection:'column'}}>
+                            {scaleList && scaleList.map((scale, index) => {
+                                if (index !== scaleList.length - 1) {
+                                    return (
+                                    <div className='margin-top-20'>
+                                        <label>{index+1}th Group</label>
+                                    </div>
+                                    );
+                                } else {
+                                    return <></>; // render nothing
+                                }
+                            })}
+                            </div>
+                            <div style={{flexDirection:'column'}}>
+                                {scaleList && scaleList.map((scale, index) => (
+                                    <div>
+                                    <input 
+                                        type='number'
+                                        key={index}
+                                        value={scaleList[index]}
+                                        onChange={(e) => {handleInputChange(e, index)}}/>
+                                    </div>
+                                ))}
+                                <button className='general-button margin-top-20'
+                                    onClick={handleInterpolation} >Interpolate</button>
+                            </div>
+                        </div>
+                    )}
+                    </div>
+
                     <div>
-                        {types && <SelectType types={types}
-                            setSelectedType={setSelectedType} />}
+                    {interpolatedRiver && <button 
+                        className='general-button margin-top-20'
+                        onClick={()=>{setMeasurementVisibility(!measurementVisibility)}}>
+                            Show/Hide Measurement Values
+                    </button>}
+                    
+                    {isLoading !== false && (<LoadingMessage />)}
+                    
+                    {interpolatedRiver && (<GoogleMap
+                        id="google-map"
+                        mapContainerStyle={mapContainerStyle}
+                        zoom={9}
+                        center={{
+                            "lng": 27.16827264000007,
+                            "lat": 41.35322476700003
+                        }}>
+
+                        {measurementVisibility && measurements && measurements.map(measurement => (
+                            <MeasurementMarker key={measurement._id} measurement={measurement} />
+                        ))}
+                        <MapComponents interpolatedRiver={interpolatedRiver} />
+                    </GoogleMap>)}
                     </div>
                 </div>
             )}
-            {interpolatedRiver && <button 
-                className='general-button margin-top-20'
-                onClick={()=>{setMeasurementVisibility(!measurementVisibility)}}>
-                    Show/Hide Measurement Values
-            </button>}
-            
-            {isLoading !== false && (<LoadingMessage />)}
-            
-            {interpolatedRiver && (<GoogleMap
-                id="google-map"
-                mapContainerStyle={mapContainerStyle}
-                zoom={9}
-                center={{
-                    "lng": 27.16827264000007,
-                    "lat": 41.35322476700003
-                }}>
-
-                {measurementVisibility && measurements && measurements.map(measurement => (
-                    <MeasurementMarker key={measurement._id} measurement={measurement} />
-                ))}
-                <MapComponents interpolatedRiver={interpolatedRiver} />
-            </GoogleMap>)}
         </div>
     )
 }
